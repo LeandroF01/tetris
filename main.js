009ad6fe537b895b1671c4056bbedc2bf8dd663c
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2024-01-11 14:16:07 -0300

    Add render board and piece of example

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
 import "./style.css";
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+const BLOCK_SIZE = 20;
+const BOARD_WIDTH = 14;
+const BOARD_HEIGHT = 30;
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH;
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+const board = createBoard(BOARD_WIDTH, BOARD_HEIGHT);
+
+function createBoard(width, height) {
+	return Array(height)
+		.fill()
+		.map(() => Array(width).fill(0));
+}
+
+const piece = {
+	position: { x: 5, y: 5 },
+	shape: [
+		[1, 1],
+		[1, 1],
+	],
+};
+
+function update() {
+	draw();
+	window.requestAnimationFrame(update);
+}
+
+function draw() {
+	context.fillStyle = "#000";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	board.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value === 1) {
+				context.fillStyle = "blue";
+				context.fillRect(x, y, 1, 1);
+			}
+		});
+	});
+	piece.shape.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value === 1) {
+				context.fillStyle = "red";
+				context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
+			}
+		});
+	});
+}
+
+document.addEventListener("keydown", (event) => {
+	if (event.key === "ArrowLeft") piece.position.x--;
+	if (event.key === "ArrowRight") piece.position.x++;
+	if (event.key === "ArrowDown") piece.position.y++;
+});
+
+update();
